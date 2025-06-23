ebfbf63f3e9e522dd3182b9ed334aa60cd333323
felona jepchumba <felonajepchumba@gmail.com>
2025-06-23 06:48:12 +0300

    Complete function definitions and fix syntax errors

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+//function declaration
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+//monday work function
+const mondayWork=function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+//wrap in function
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
+
